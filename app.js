4504c9e2bc0fa69f315431c5175189b2d009bc4c
okg21 <ogurbuz@etu.edu.tr>
2024-01-30 00:32:03 +0300

    style changes, graph bugfix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var neo4j = require('neo4j-driver');
 var cytoscape = require('cytoscape');
 var fcose = require('cytoscape-fcose');
 
-cytoscape.use( fcose ); 
+cytoscape.use(fcose);
 
 
 // Initialize the app
@@ -37,58 +37,59 @@ var dataArr = [];
 
 
 // Page Routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index');
-    });
+});
 
 
-app.post('/addActorMovies', async function(req, res) {
+app.post('/addActorMovies', async function (req, res) {
     var actorId = parseInt(req.body.actorId);
 
     // Create a session for this specific request
     var session = driver.session();
+    //empty data array
+    var newDataArr = [];
 
     try {
         // Parameterize your query to prevent injection attacks
         var query = 'MATCH (actor:Person)-[edge:ACTED_IN]->(movie:Movie) WHERE ID(actor) = ' + actorId + ' RETURN movie, edge';
         var result = await session.run(query);
 
-        result.records.forEach(function(record) {
+        result.records.forEach(function (record) {
             edge = record._fields[1];
-                //if not inside the movieArr add it to movieArr and data
-                if (!movieArr.includes(record._fields[0].properties.title)) {
-                    movieArr.push(record._fields[0].properties.title);
+            //if not inside the movieArr add it to movieArr and data
+            if (!movieArr.includes(record._fields[0].properties.title)) {
+                movieArr.push(record._fields[0].properties.title);
 
-                    //add nodes to data array 
-                    dataArr.push({
-                        data: {
-                            id: record._fields[0].identity.low,
-                            type: record._fields[0].labels[0],
-                            properties: record._fields[0].properties,
-                        }
-                    });
-                    
-                    //add edges to data array 
-                    var edgeId = edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity;
-                    if (dataArr.some(e => e.data.id === edgeId)) {
-                        return;
+                //add nodes to data array 
+                newDataArr.push({
+                    data: {
+                        id: record._fields[0].identity.low,
+                        type: record._fields[0].labels[0],
+                        properties: record._fields[0].properties,
                     }
-                    dataArr.push({
-                        data: {
-                            id: edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity, 
-                            source: edge.start.low,
-                            target: edge.end.low,
-                            type: edge.type,
-                            label: edge.type,
-                            properties: edge.properties,
-                        }
-                    });
-                    //print dataArr length
-                    console.log(dataArr.length);
+                });
+
+                //add edges to data array 
+                var edgeId = edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity;
+                if (newDataArr.some(e => e.data.id === edgeId)) {
+                    return;
                 }
+                newDataArr.push({
+                    data: {
+                        id: edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity,
+                        source: edge.start.low,
+                        target: edge.end.low,
+                        type: edge.type,
+                        label: edge.type,
+                        properties: edge.properties,
+                    }
+                });
+
+            }
         });
         // Send the response
-        res.json(dataArr);
+        res.json(newDataArr);
     } catch (error) {
         // Handle any errors that occurred
         console.log(error);
@@ -99,53 +100,52 @@ app.post('/addActorMovies', async function(req, res) {
     }
 });
 
-app.post('/addMovieActors', async function(req, res) {
+app.post('/addMovieActors', async function (req, res) {
     var movieId = parseInt(req.body.movieId);
 
     // Create a session for this specific request
     var session = driver.session();
+    //empty data array
+    var newDataArr = [];
 
     try {
-        // Parameterize your query to prevent injection attacks
         var query = 'MATCH (actor:Person)-[edge:ACTED_IN]->(movie:Movie) WHERE ID(movie) = ' + movieId + ' RETURN actor, edge';
         var result = await session.run(query);
 
-        result.records.forEach(function(record) {
+        result.records.forEach(function (record) {
             edge = record._fields[1];
-                //if not inside the movieArr add it to movieArr and data
-                if (!actorArr.includes(record._fields[0].properties.title)) {
-                    actorArr.push(record._fields[0].properties.title);
+            //if not inside the movieArr add it to movieArr and data
+            if (!actorArr.includes(record._fields[0].properties.name)) {
+                actorArr.push(record._fields[0].properties.name);
 
-                    //add nodes to data array 
-                    dataArr.push({
-                        data: {
-                            id: record._fields[0].identity.low,
-                            type: record._fields[0].labels[0],
-                            properties: record._fields[0].properties,
-                        }
-                    });
-                    
-                    //add edges to data array 
-                    var edgeId = edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity;
-                    if (dataArr.some(e => e.data.id === edgeId)) {
-                        return;
+                //add nodes to data array 
+                newDataArr.push({
+                    data: {
+                        id: record._fields[0].identity.low,
+                        type: record._fields[0].labels[0],
+                        properties: record._fields[0].properties,
                     }
-                    dataArr.push({
-                        data: {
-                            id: edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity, 
-                            source: edge.start.low,
-                            target: edge.end.low,
-                            type: edge.type,
-                            label: edge.type,
-                            properties: edge.properties,
-                        }
-                    });
-                    //print dataArr length
-                    console.log(dataArr.length);
+                });
+
+                //add edges to data array 
+                var edgeId = edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity;
+                if (newDataArr.some(e => e.data.id === edgeId)) {
+                    return;
                 }
+                newDataArr.push({
+                    data: {
+                        id: edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity,
+                        source: edge.start.low,
+                        target: edge.end.low,
+                        type: edge.type,
+                        label: edge.type,
+                        properties: edge.properties,
+                    }
+                });
+            }
         });
         // Send the response
-        res.json(dataArr);
+        res.json(newDataArr);
     } catch (error) {
         // Handle any errors that occurred
         console.log(error);
@@ -156,14 +156,16 @@ app.post('/addMovieActors', async function(req, res) {
     }
 });
 
-app.post('/actor/depth', async function(req, res) {
+app.post('/actor/depth', async function (req, res) {
     //get actor name and depth from the request
     var actorName = req.body.actor;
     var depth = parseInt(req.body.depth) * 2;
-    
-    //empty data array
+
+    //empty all arrays
+    movieArr = [];
+    actorArr = [];
     dataArr = [];
-    
+
     // Create a session for this specific request
     var session = driver.session();
 
@@ -173,7 +175,7 @@ app.post('/actor/depth', async function(req, res) {
             'MATCH p = (actor:Person {name:"' + actorName + '"})-[:ACTED_IN*0..' + depth + ']-(entity) WITH *, relationships(p) AS edge RETURN entity, edge');
 
         // Process the result
-        result.records.forEach(function(record) {
+        result.records.forEach(function (record) {
             //add title and actor names to arrays
             if (record._fields[0].labels[0] === 'Movie') {
                 movieArr.push(record._fields[0].properties.title);
@@ -192,7 +194,7 @@ app.post('/actor/depth', async function(req, res) {
 
             //add edges to data array in the cytoscape format
             if (record._fields[1].size != 0) {
-                record._fields[1].forEach(function(edge) {
+                record._fields[1].forEach(function (edge) {
                     //if it is already pushed continue
                     var edgeId = edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity;
                     if (dataArr.some(e => e.data.id === edgeId)) {
@@ -200,7 +202,7 @@ app.post('/actor/depth', async function(req, res) {
                     }
                     dataArr.push({
                         data: {
-                            id: edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity, 
+                            id: edge.start.low + '-' + edge.type + '-' + edge.end.low + '_' + edge.identity,
                             source: edge.start.low,
                             target: edge.end.low,
                             type: edge.type,
